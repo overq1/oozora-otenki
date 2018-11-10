cb2a3401afe8fa081532403eec57d8fb0ca06188
overq1 <overq1@gmail.com>
2018-11-10 17:52:42 +0900

    promise

--- index.js
+++ index.js
@@ -29,55 +29,53 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
 
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
-    var msg = '';
 
-    // イベントオブジェクトを順次処理。
+        // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
             if (event.message.text == "hello"){
-                // http request
-                events_processed.push(http.get(url, (weather_res) => {
-                    var body = '';
-                    weather_res.setEncoding('utf8');
-                    weather_res.on('data', (chunk) => {
-                        body += chunk
-                    });
-                    weather_res.on('end', (weather_res) => {
-                        weather_res = JSON.parse(body);
-                        var city = weather_res.location.city;
-                        var date = weather_res.forecasts[0].dateLabel;
-                        var weather = weather_res.forecasts[0].telop
-                        msg = city + "の" + date + 'のお天気は' + weather + 'です';
+               getWeather().then(function(msg) {
+                   bot.replyMessage(event.replyToken, {
+                       type: "text",
+                       text: msg,
+                   })
+               });
 
-                        // 気温もわかる場合
-                        if (weather_res.forecasts[0].temperature.max) {
-                            max_temperature = weather_res.forecasts[0].temperature.max.celsius;
-                            min_temperature = weather_res.forecasts[0].temperature.min.celsius;
-                            msg += "\n";
-                            msg += "最高気温は" + max_temperature + "度で";
-                            msg += "最低気温は" + min_temperature + "度です";
-                        }
-
-                        console.log(msg);
-                    })
-                }));
-
-               // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-              events_processed.push(bot.replyMessage(event.replyToken, {
-                type: "text",
-                text: "hello"
-              }));
             }
         }
     });
-
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
 });
 
+function getWeather() {
+    return new Promise((resolve, reject) => 
+        http.get(url, (weather_res) => {
+            var msg =  '';
+            var body = '';
+            weather_res.setEncoding('utf8');
+            weather_res.on('data', (chunk) => {
+                body += chunk
+            });
+            weather_res.on('end', (weather_res) => {
+                weather_res = JSON.parse(body);
+                var city = weather_res.location.city;
+                var date = weather_res.forecasts[0].dateLabel;
+                var weather = weather_res.forecasts[0].telop
+                msg = city + "の" + date + 'のお天気は' + weather + 'です';
+        
+                // 気温もわかる場合
+                if (weather_res.forecasts[0].temperature.max) {
+                    max_temperature = weather_res.forecasts[0].temperature.max.celsius;
+                    min_temperature = weather_res.forecasts[0].temperature.min.celsius;
+                    msg += "\n";
+                    msg += "最高気温は" + max_temperature + "度で";
+                    msg += "最低気温は" + min_temperature + "度です";
+                }
+        
+                console.log(msg);
+            })
+            return msg;
+        })
+    );
+}
